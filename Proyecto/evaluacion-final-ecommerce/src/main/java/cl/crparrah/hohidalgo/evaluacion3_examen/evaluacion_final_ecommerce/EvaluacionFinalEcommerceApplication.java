a7ffca24a605770b917dfb21f38b68d8d9959e01
Chainsaw Sadomator <109769365+MACHINE-GUN-SADOMY@users.noreply.github.com>
2025-06-18 22:03:56 -0400

    Configuración inicial del proyecto con Spring Boot 3 y H2 DB
    Este commit establece la estructura base del proyecto, utilizando Spring Boot 3.4.6.
    **Se requiere el uso de Java 23 para el correcto funcionamiento y compilación del proyecto.**
    
    Se han integrado las siguientes dependencias clave para el desarrollo inicial:
    - `Spring Web`: Para la construcción de APIs RESTful.
    - `Spring Boot Starter Validation`: Para la validación de datos de entrada en los endpoints.
    - `H2 Database`: Utilizada como base de datos en memoria/archivo para el entorno de desarrollo.
    - `Lombok`: Para reducir el código repetitivo (boilerplate) en las clases (getters, setters, constructores).
    - `Spring Boot DevTools`: Para facilitar el ciclo de desarrollo con reinicios automáticos de la aplicación.
    - `Spring Boot Starter Test`: Para la creación y ejecución de pruebas unitarias y de integración.
    
    Co-Authored-By: Horacio A. Hidalgo Araya <107312341+horaciohidalgo@users.noreply.github.com>

---
+++ Proyecto/evaluacion-final-ecommerce/src/main/java/cl/crparrah/hohidalgo/evaluacion3_examen/evaluacion_final_ecommerce/EvaluacionFinalEcommerceApplication.java
@@ -0,0 +1,13 @@
+package cl.crparrah.hohidalgo.evaluacion3_examen.evaluacion_final_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaluacionFinalEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaluacionFinalEcommerceApplication.class, args);
+	}
+
+}
---
+++ Proyecto/evaluacion-final-ecommerce/src/test/java/cl/crparrah/hohidalgo/evaluacion3_examen/evaluacion_final_ecommerce/EvaluacionFinalEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.crparrah.hohidalgo.evaluacion3_examen.evaluacion_final_ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaluacionFinalEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
