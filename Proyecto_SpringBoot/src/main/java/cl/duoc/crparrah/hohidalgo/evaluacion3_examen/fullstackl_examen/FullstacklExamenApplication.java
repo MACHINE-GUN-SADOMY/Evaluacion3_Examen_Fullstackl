92a1bde44d809c63c5f1def37d0bb7c22a137270
Chainsaw Sadomator <109769365+MACHINE-GUN-SADOMY@users.noreply.github.com>
2025-06-29 00:20:29 -0400

    Actualizada la base del proyecto a SpringBoot

---
+++ Proyecto_SpringBoot/src/main/java/cl/duoc/crparrah/hohidalgo/evaluacion3_examen/fullstackl_examen/FullstacklExamenApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.crparrah.hohidalgo.evaluacion3_examen.fullstackl_examen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstacklExamenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstacklExamenApplication.class, args);
+	}
+
+}
---
+++ Proyecto_SpringBoot/src/test/java/cl/duoc/crparrah/hohidalgo/evaluacion3_examen/fullstackl_examen/FullstacklExamenApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.crparrah.hohidalgo.evaluacion3_examen.fullstackl_examen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstacklExamenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
