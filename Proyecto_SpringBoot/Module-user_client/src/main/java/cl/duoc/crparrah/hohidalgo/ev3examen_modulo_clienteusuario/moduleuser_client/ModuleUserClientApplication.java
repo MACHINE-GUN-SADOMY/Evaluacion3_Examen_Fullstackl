3f1f7cfc92ab417380df97d7bb68fd0c7c31deb4
Chainsaw Sadomator <109769365+MACHINE-GUN-SADOMY@users.noreply.github.com>
2025-06-29 01:18:47 -0400

    Configuracion completa para H2 Modulo user-client
    Debido a un error por mi parte, elimine completamente el proyecto antiguo, por lo que nuevamente, seran creadas las JPA y lo demas

---
+++ Proyecto_SpringBoot/Module-user_client/src/main/java/cl/duoc/crparrah/hohidalgo/ev3examen_modulo_clienteusuario/moduleuser_client/ModuleUserClientApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.crparrah.hohidalgo.ev3examen_modulo_clienteusuario.moduleuser_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleUserClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleUserClientApplication.class, args);
+    }
+
+}
---
+++ Proyecto_SpringBoot/Module-user_client/src/test/java/cl/duoc/crparrah/hohidalgo/ev3examen_modulo_clienteusuario/moduleuser_client/ModuleUserClientApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.crparrah.hohidalgo.ev3examen_modulo_clienteusuario.moduleuser_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleUserClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
